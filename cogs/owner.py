7dd56a9012eca9f3f33b5019bda24f2efdfc8fe7
Benjamin Palmu <neverquitben@gmail.com>
2019-04-25 08:34:35 +0900

    gitignore

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import imageio
+import os
+import numpy
 
 class ownerCog(commands.Cog):
     def __init__(self, bot):
@@ -19,6 +22,24 @@ class ownerCog(commands.Cog):
             await ctx.send(f"```\n✅ Evaluated successfully:\n{result}\n```")
         else:
             await ctx.send(f"```\n🚫 An exception occurred:\n{result}\n```")
+
+    @commands.command(name="reloademotes",)
+    @commands.is_owner()
+    async def refillemotecache(self, ctx):
+        objectFrames = {}
+        spriteNames = os.listdir("sprites") # Outputs are sadly not in order
+        # Establishes dictionary keys
+        for sprite in spriteNames:
+            segments = objectFrames[sprite.split("_")]
+            if segments[0] == "text":
+                image = imageio.imread("sprites/" + sprite, format="PNG")
+                if objectFrames.get(segments[1]) == None:
+                    objectFrames[segments[1]] = [image]
+                else:
+                    objectFrames[segments[1]].append(image)
+
+
+
 
 def setup(bot):
     bot.add_cog(ownerCog(bot))
