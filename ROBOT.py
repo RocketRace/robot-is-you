ce40f9cc33095b8c775be3f840991fd1ab83c9ce
Olivia Palmu <oliviaspalmu@gmail.com>
2019-10-04 10:30:23 +0900

    Useable in direct messages

--- ROBOT.py
+++ ROBOT.py
@@ -71,6 +71,8 @@ async def reloadcog(ctx, cog: str):
         await ctx.send(f"Reloaded extension `{cog}` from `cogs/{cog}.py`.")
 
 def prefix_whitelist(ctx):
+    if ctx.guild:
+        return True
     if ctx.prefix == "+" and ctx.guild.id == 264445053596991498:
         return False
     return True
