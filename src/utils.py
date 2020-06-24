1eef6d935dd69f5d5a7d37d5c73552326212c4fb
RocketRace <neverquitben@gmail.com>
2020-06-24 09:33:16 +0900

    Cache opens

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
+from PIL import Image
+
+__all__ = ("Tile", "cached_open")
 
 class Tile:
+    '''
+    Represents a tile object, ready to be rendered.
+    '''
     def __init__(self, name = None, variant = None, color = None, source = "vanilla", images = []):
         self.name = name
         self.variant = variant
@@ -12,3 +18,13 @@ class Tile:
         if self.custom:
             return f"<Custom tile {self.name}>"
         return f"<Tile {self.name} : {self.variant} with {self.color} from {self.source}>"
+
+def cached_open(path, *, cache, is_image=False):
+    '''
+    Checks whether a path is in the cache, and if so, returns that element. Otherwise calls open() or Image.open() on the path. 
+    '''
+    if path in cache:
+        return cache[path]
+    result = Image.open(path) if is_image else open(path)
+    cache[path] = result
+    return result
